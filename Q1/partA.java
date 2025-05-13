e5a5c2918f635c4ff905ff1e203323bcdbd8ced8
Bintao Chi <157766746+Cccccbt@users.noreply.github.com>
2025-05-13 09:54:01 -0400

    Finalize partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,16 @@
+public int walkDogs(int hour)
+    {
+        int companyMax = company.numAvailableDogs(hour);
 
+        if (maxDogs > companyMax)
+        {
+            company.updateDogs(companyMax);
+            return companyMax;
+        }
+
+        else
+        {
+            company.updateDogs(max);
+            return max;
+        }
+    }
